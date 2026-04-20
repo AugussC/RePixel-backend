086bc26efc5847378a5ac8afa88fc282644f8a3c
Toia <victoriacool124@gmail.com>
2026-04-20 20:04:56 -0300

    error

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
-from app.routes import user_routes
+
+from app.routes.user_routes import user_routes
 from app.routes.auth_routes import auth_routes
-app.register_blueprint(user_routes)
+
 
 app = Flask(__name__)
 app.secret_key = "drmerygraciasportodalainfo"
 CORS(app)  
 
-app.register_blueprint(auth_routes, user_routes)
+app.register_blueprint(auth_routes)
+app.register_blueprint(user_routes)
 
 if __name__ == "__main__":
     app.run(debug=True)
